e65fd7ea2e2ee52d8eeabe50c22192f483cbe707
JekoGyulev <165460153+JekoGyulev@users.noreply.github.com>
2024-07-12 19:04:47 +0300

    Update Transaction.java

--- bankatm/Transaction.java
+++ bankatm/Transaction.java
@@ -1,4 +1,38 @@
 package my_github_projects.bankatm;
 
 public class Transaction {
+   private double amount;
+    private Date timestamp;
+    private String memo;
+
+    //The account in which the transaction was made.
+    private Account inAccount;
+
+    public Transaction(double amount, Account inAccount) {
+        this.amount = amount;
+        this.inAccount = inAccount;
+        this.timestamp = new Date();
+        this.memo = "";
+    }
+
+    public Transaction(double amount, String memo, Account account) {
+        // call the two-arg constructor first
+        this(amount, account);
+        //set the memo
+        this.memo = memo;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public String getSummaryLine() {
+        if (this.amount >= 0) {
+            return String.format("%s : $%.2f : %s", this.timestamp.toString(),
+                   this.amount, this.memo);
+        } else {
+            return String.format("%s : $(%.2f) : %s", this.timestamp.toString(),
+                    -this.amount, this.memo);
+        }
+    }
 }
